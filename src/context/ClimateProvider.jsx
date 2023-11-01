44017e32ebb52c10130fb551b198e938ae8dbade
gabicilfone <cilfonegabriel@gmail.com>
2023-11-01 12:26:39 -0300

    Checking the API weather

--- src/context/ClimateProvider.jsx
+++ src/context/ClimateProvider.jsx
@@ -1,8 +1,10 @@
 import { useState, createContext } from "react";
+import axios from "axios";
 
 const ClimateContext = createContext()
 const ClimateProvider = ({children}) => {
 
+    const [result, setResult] = useState({})
 
     const [search, setSearch] = useState({
         city: '',
@@ -16,8 +18,23 @@ const ClimateProvider = ({children}) => {
         })
     }
 
-    const checkClimate = datos => {
-        console.log(datos)
+    const checkClimate = async datos => {
+        try {
+            const { city, country } = datos
+            const appId = import.meta.env.VITE_API_KEY
+
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=${appId}`
+            const { data } = await axios(url)
+            const { lat, lon } = data[0]
+
+            const urlClimate = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+
+            const { data: climate } = await axios(urlClimate)
+            setResult(climate)
+
+        } catch (err) {
+            console.error(err)
+        }
     }
     
     return(
@@ -25,7 +42,8 @@ const ClimateProvider = ({children}) => {
             value={{
                 search,
                 datosSearch,
-                checkClimate
+                checkClimate,
+                result
             }}
         >
             {children}
