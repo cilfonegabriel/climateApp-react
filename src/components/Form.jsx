407a780c781ea3aa5490e92c5442616ac11ca680
gabicilfone <cilfonegabriel@gmail.com>
2023-11-01 11:11:47 -0300

    Creating the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,44 @@
 const Form = () => {
   return (
     <div>
-      Desde formulario
+        <div className="contenedor">
+            <form >
+                <div className="campo">
+                    <label htmlFor="city">City</label>
+                    <input 
+                        type="text" 
+                        id="city"
+                        name="city"
+                    />
+                </div>
+
+                <div className="campo">
+                    <label htmlFor="country">Country</label>
+                    <select 
+                        name="country" 
+                        id="country"
+                    >
+                        <option value=""> Select a country </option>
+                        <option value="US">United State</option>
+                        <option value="MX">México</option>
+                        <option value="AR">Argentina</option>
+                        <option value="CO">Colombia</option>
+                        <option value="CR">Costa Rica</option>
+                        <option value="PE">Perú</option>
+                        <option value="ES">España</option>
+                        <option value="BR">Brasil</option>
+                        <option value="CL">Chile</option>
+                        <option value="PY">Paraguay</option>
+                        <option value="UY">Uruguay</option>
+                        <option value="IT">Italia</option>
+                    </select>
+                </div>
+                <input 
+                    type="submit"
+                    value="Check weather" 
+                />
+            </form>
+        </div>
     </div>
   )
 }
